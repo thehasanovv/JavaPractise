9f4f975af84a730301950dd8792bf722737803a3
thehasanovv <hasanov.e95@gmail.com>
2024-06-13 00:23:19 +0400

    Problem #8940

---
+++ src/eolymp/Rectangle3.java
@@ -0,0 +1,20 @@
+package eolymp;
+
+import java.util.Scanner;
+
+public class Rectangle3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 1; j < b; j++) {
+                System.out.print("#");
+            }
+            System.out.println("#");
+        }
+    }
+}
