26bc55d26f5aee4b6c15e76ea3098d106f26b682
thehasanovv <hasanov.e95@gmail.com>
2024-06-13 00:33:23 +0400

    Problem #8242

---
+++ src/eolymp/PositiveNegativeOrZero.java
@@ -0,0 +1,15 @@
+package eolymp;
+
+import java.util.Scanner;
+
+public class PositiveNegativeOrZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
+
+        if (num > 0) System.out.println("Positive");
+        else if (num < 0) System.out.println("Negative");
+        else System.out.println("Zero");
+    }
+}
