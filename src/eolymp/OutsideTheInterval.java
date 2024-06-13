a3ac2340c49bd5943308996af1e0afa21b34aef1
thehasanovv <hasanov.e95@gmail.com>
2024-06-14 02:30:03 +0400

    Problem #8615

---
+++ src/eolymp/OutsideTheInterval.java
@@ -0,0 +1,20 @@
+package eolymp;
+
+import java.util.Scanner;
+
+public class OutsideTheInterval {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        sc.close();
+        System.out.println(x < y || x > z);
+
+        if (x < y || x > z)
+            System.out.println("OUT");
+        else
+            System.out.println("IN");
+    }
+}
