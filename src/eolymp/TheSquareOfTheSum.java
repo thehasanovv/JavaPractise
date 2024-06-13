e6c14048c11497180d58f7621e004df3187a0e19
thehasanovv <hasanov.e95@gmail.com>
2024-06-14 02:47:13 +0400

    Problem #939

---
+++ src/eolymp/TheSquareOfTheSum.java
@@ -0,0 +1,12 @@
+package eolymp;
+
+import java.util.Scanner;
+
+public class TheSquareOfTheSum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        int sumOfDigits = number / 10 + number % 10;
+        System.out.print(sumOfDigits * sumOfDigits);
+    }
+}
