4a82624308fd07e2ae01209b339dd26ae5e27f03
thehasanovv <hasanov.e95@gmail.com>
2024-06-14 17:29:13 +0400

    Problem #1607

---
+++ src/eolymp/TheNumberInReverseOrder.java
@@ -0,0 +1,20 @@
+package eolymp;
+
+import java.util.Scanner;
+
+public class TheNumberInReverseOrder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if (num == 0) {
+            System.out.println(0);
+        }
+
+        while (num > 0) {
+            int digits = num % 10;
+            num /= 10;
+            System.out.println(digits);
+        }
+    }
+}
