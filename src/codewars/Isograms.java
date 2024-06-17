d4d44974104f83d91cee9b63ba14eca6756a7a02
thehasanovv <hasanov.e95@gmail.com>
2024-06-18 00:31:27 +0400

    7kyu

---
+++ src/codewars/Isograms.java
@@ -0,0 +1,29 @@
+package codewars;
+
+public class Isograms {
+    public static void main(String[] args) {
+        boolean test1 = isIsogram("isIsogram");
+        boolean test2 = isIsogram("");
+
+        System.out.println(test1);
+        System.out.println(test2);
+    }
+
+    public static boolean isIsogram(String str) {
+        String tempStr = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            String s = String.valueOf(str.charAt(i)).toLowerCase();
+
+            if (!tempStr.contains(s)) tempStr += s;
+            else return false;
+        }
+
+        return true;
+    }
+}
+/*
+    public static boolean  isIsogram(String str) {
+        return str.toLowerCase().chars().distinct().count() == str.length();
+    };
+*/
