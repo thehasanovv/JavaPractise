3374d1c0e22493bba9c899633adf5ce66c8866b0
thehasanovv <hasanov.e95@gmail.com>
2024-06-25 19:58:00 +0400

    8kyu

---
+++ src/codewars/GrasshopperPersonalizedMessage.java
@@ -0,0 +1,15 @@
+package codewars;
+
+public class GrasshopperPersonalizedMessage {
+    public static void main(String[] args) {
+        String test1 = greet("Daniel", "Daniel");
+        String test2 = greet("Greg", "Daniel");
+
+        System.out.println(test1);
+        System.out.println(test2);
+    }
+
+    private static String greet(String name, String owner) {
+        return "Hello " + (name == owner ? "boss" : "guest");
+    }
+}
--- src/codewars/TotalAmountOfPoints.java
+++ src/codewars/TotalAmountOfPoints.java
@@ -5,12 +5,20 @@ import java.util.Arrays;
 public class TotalAmountOfPoints {
     public static void main(String[] args) {
         String[] matchResults = new String[]{"3:1", "2:2", "0:1", "4:0", "1:1", "3:3", "2:0", "1:2", "2:1", "1:0"};
-        int totalPoints = points(matchResults);
+        int totalPoints = points2(matchResults);
         System.out.println("Total Points: " + totalPoints);
     }
 
-    public static int points(String[] games) {
+    private static int points(String[] games) {
         return Arrays.stream(games)
+                .mapToInt(r -> r.charAt(0) - r.charAt(2))
+                .map(m -> m > 0 ? 3 : m == 0 ? 1 : 0)
+                .sum();
+    }
+
+    private static int points2(String[] games) {
+        return Arrays.asList(games)
+                .stream()
                 .mapToInt(r -> r.charAt(0) - r.charAt(2))
                 .map(m -> m > 0 ? 3 : m == 0 ? 1 : 0)
                 .sum();
